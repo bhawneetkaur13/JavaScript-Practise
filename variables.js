538932510e9d9990942a9cc99955d423546e4520
bhawneet <bhawneetkaur015@gmail.com>
2022-09-08 18:41:27 +0530

    Added first commit

---
+++ variables.js
@@ -0,0 +1,26 @@
+// Variable is a container that stores some value
+// the variable behaves like if it was the value that it contains
+
+// Two steps
+// 1. Declaration(let,const,var)  let is the best practise because of variable scope
+// 2. Assignment (= operator)
+
+// Data Types
+// 1. number (can be used in arithmetic operations)
+// 2. string (series of characters)
+// 3. boolean (true or false)
+
+
+let age = 21;  //it is not compulsory to use semicolon
+console.log(age);
+
+let firstName = "bhawneet"
+console.log(firstName);
+
+let status1 = true
+console.log(status1);
+
+// To display the content on DOM
+document.getElementById("p1").innerHTML = "Hello " + firstName;  //during string concatenation take care of spacing
+document.getElementById("p2").innerHTML = "My age is " + age;
+document.getElementById("p3").innerHTML = "Enrolled: " + status1;
